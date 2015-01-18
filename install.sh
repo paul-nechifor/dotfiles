24ebf6d009b555ac39bc5ca78d54fd80dd07381d
Paul Nechifor <paul@nechifor.net>
2015-01-18 12:03:07 +0000

    Add more plugins.

--- install.sh
+++ install.sh
@@ -190,10 +190,11 @@ install_vim_modules() {
   install_from_github javascript pangloss/vim-javascript
   install_from_github markdown tpope/vim-markdown
   install_from_github matchem ervandew/matchem
-  install_from_github move matze/vim-move
   install_from_github nerdtree scrooloose/nerdtree
+  install_from_github python-mode klen/python-mode
   install_from_github stylus wavded/vim-stylus
   install_from_github supertab ervandew/supertab
+  install_from_github textmanip t9md/vim-textmanip
 }
 
 provision_vim() {
