e8fbd2e3a929d6ccb597f1fd87afd48b70d16fdf
Paul Nechifor <paul@nechifor.net>
2015-01-21 19:32:20 +0000

    Remove tmux installation as it doesn't work on FreeBSD.

--- provision/freebsd.sh
+++ provision/freebsd.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-tmux=tmux-1.8
-ncurses=ncurses-5.9
-libevent=libevent-2.0.19-stable
-
 install=$HOME/.local-installs
 provision=$HOME/provision-tmp
 
@@ -24,32 +20,6 @@ teardown() {
   touch ~/.freebsd-provisioned
 }
 
-install_tmux() {
-  cd "$provision"
-
-  wget -O ${tmux}.tar.gz http://sourceforge.net/projects/tmux/files/tmux/${tmux}/${tmux}.tar.gz/download
-  wget -O ${libevent}.tar.gz http://sourceforge.net/projects/levent/files/libevent/libevent-2.0/${libevent}.tar.gz/download
-  wget http://ftp.gnu.org/gnu/ncurses/${ncurses}.tar.gz
-
-  tar xvzf ${libevent}.tar.gz
-  cd $libevent
-  ./configure --prefix=$install --disable-shared
-  make && make install
-  cd ..
-
-  tar xvzf ${ncurses}.tar.gz
-  cd $ncurses
-  ./configure --prefix=$install
-  make && make install
-  cd ..
-
-  tar xvzf ${tmux}.tar.gz
-  cd $tmux
-  ./configure CFLAGS="-I$install/include -I$install/include/ncurses" LDFLAGS="-L$install/lib -L$install/include/ncurses -L$install/include" CPPFLAGS="-I$install/include -I$install/include/ncurses" LDFLAGS="-static -L$install/include -L$install/include/ncurses -L$install/lib"
-  make
-  cp tmux $install/bin
-}
-
 install_vim() {
   cd "$provision"
   wget --no-check-certificate https://github.com/b4winckler/vim/archive/master.zip
@@ -64,7 +34,6 @@ install_vim() {
 main() {
   setup
   install_vim
-  install_tmux
   teardown
 }
 
