5961bf901c6db9abff5450109433c557a9c4889b
Paul Nechifor <paul@nechifor.net>
2015-04-11 13:17:00 +0100

    Build Python 2.7.

--- provision/freebsd.sh
+++ provision/freebsd.sh
@@ -4,7 +4,7 @@ set -e
 
 lock_file="$HOME/local/.freebsd-provisioned"
 install_path="$HOME/local/.ownbin"
-provision=$(mktemp -dt provision)
+tmpdir=$(mktemp -dt provision)
 
 main() {
   setup
@@ -27,24 +27,40 @@ setup() {
       mkdir -p "$HOME/local"
     fi
   fi
-
-  rm -fr "$install_path"
-  mkdir -p "$install_path" 2>/dev/null || true
 }
 
 teardown() {
-  rm -fr "$provision"
+  rm -fr "$tmpdir"
   touch "$lock_file"
 }
 
 install_all() {
+  rm -fr "$install_path"
+  mkdir -p "$install_path" 2>/dev/null || true
+  install_python
   install_vim
 }
 
+install_python() {
+  local version=2.7.9
+
+  cd "$tmpdir"
+  wgetf https://www.python.org/ftp/python/$version/Python-${version}.tgz Python.tgz
+  tar -xzf Python.tgz
+  cd Python-$version
+  ./configure --prefix="$install_path"
+  make && make altinstall
+
+  wgetf https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py ez_setup.py
+  "$install_path/bin/python2.7" ez_setup.py
+  "$install_path/bin/easy_install-2.7" pip
+  "$install_path/bin/pip2.7" install virtualenv
+}
+
 install_vim() {
-  cd "$provision"
+  cd "$tmpdir"
   local vim_source="https://github.com/b4winckler/vim/archive/master.zip"
-  wget -q --no-check-certificate "$vim_source" -O- > master.zip
+  wgetf "$vim_source" master.zip
   unzip master.zip
   cd vim-master
   export LDFLAGS="-static"
@@ -59,6 +75,10 @@ install_vim() {
   make install
   cd ..
   rm -fr vim-master
+}
+
+wgetf() {
+  wget -q --no-check-certificate "$1" -O- > "$2"
 }
 
 main
