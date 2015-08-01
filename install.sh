cad558689d8feacae153b1ddac8b226015f9bcd0
Paul Nechifor <paul@nechifor.net>
2015-08-01 14:17:00 +0100

    Finally add Git Fugitive.

--- install.sh
+++ install.sh
@@ -237,6 +237,7 @@ install_vim_modules() {
   install_from_github commentary tpope/vim-commentary
   install_from_github ctrlp kien/ctrlp.vim
   install_from_github easymotion Lokaltog/vim-easymotion
+  install_from_github fugitive tpope/vim-fugitive
   install_from_github gitgutter airblade/vim-gitgutter
   install_from_github jade digitaltoad/vim-jade
   install_from_github javascript pangloss/vim-javascript
