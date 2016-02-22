7bb71a0dcfcb390613ade47b64aaf9d24a4e6676
Paul Nechifor <paul@nechifor.net>
2016-02-22 09:33:51 +0000

    Remove Jedi Vim.

--- install.sh
+++ install.sh
@@ -270,7 +270,6 @@ install_vim_modules() {
   install_from_github gitgutter airblade/vim-gitgutter
   install_from_github jade digitaltoad/vim-jade
   install_from_github javascript pangloss/vim-javascript
-  install_from_github jedi davidhalter/jedi-vim
   install_from_github markdown tpope/vim-markdown
   install_from_github matchem ervandew/matchem
   install_from_github nerdtree scrooloose/nerdtree
